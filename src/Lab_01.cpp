034a8af7264e1a98282078fbe3c59650437b4fea
flor <flordelombaerde@hotmail.com>
2025-11-10 14:12:16 +0100

    Lab01 initial files

---
+++ src/Lab_01.cpp
@@ -0,0 +1,47 @@
+#include "Lab_01.h"
+
+/**
+ * Normalizing a multivector
+ * @param multi Input multivector
+ * @return Normalized multivector
+ */
+MultiVector Exercise_00(MultiVector multi)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+/**
+ * Creates a point on the intersection of three planes.
+ * @param plane1
+ * @param plane2
+ * @param plane3
+ * @return Intersection point of three planes
+ */
+TriVector Exercise_01(Vector plane1, Vector plane2, Vector plane3)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+/**
+ * Generate a normalized pluckerline going through two given points
+ * @param point1
+ * @param point2
+ * @return Bivector pluckerline
+ */
+BiVector Exercise_02(TriVector point1, TriVector point2)
+{
+    throw std::runtime_error("Not implemented");
+}
+
+/**
+ * Generates a plane going through a point with a certain normal
+ * @param point
+ * @param x x-coordinate of the plane normal
+ * @param y y-coordinate of the plane normal
+ * @param z z-coordinate of the plane normal
+ * @return Normalized plane
+ */
+Vector Exercise_03(TriVector point, float x, float y, float z)
+{
+    throw std::runtime_error("Not implemented");
+}
---
+++ src/Lab_01.h
@@ -0,0 +1,6 @@
+#include "../FlyFish/src/FlyFish.h"
+
+MultiVector Exercise_00(MultiVector multi);
+TriVector Exercise_01(Vector plane1, Vector plane2, Vector plane3);
+BiVector Exercise_02(TriVector point1, TriVector point2);
+Vector Exercise_03(TriVector point, float x, float y, float z);
