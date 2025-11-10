b234a9dc12e0ded6ac47330a4f43ee4e11a0794a
flor <flordelombaerde@hotmail.com>
2025-11-10 15:47:05 +0100

    Add description to the assignment 6 method.

--- src/Ass_06.cpp
+++ src/Ass_06.cpp
@@ -1,5 +1,13 @@
 #include "Ass_06.h"
 
+/**
+ * Find the closest point to a plane using geometric algebra
+ * @param plane Vector representing a plane
+ * @param point1 First point
+ * @param point2 Second point
+ * @return The input trivector with the smallest euclidean distance to the input plane. In case the distances are
+ * the same, return the first input point.
+ */
 TriVector ClosestPointToPlane(Vector plane, TriVector point1, TriVector point2)
 {
     throw std::runtime_error("Not implemented");
